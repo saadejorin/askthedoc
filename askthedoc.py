01aa4f44ab66c46916478531aef51a88f3bc89a0
dev@flux <saadejorin@gmail.com>
2023-06-29 13:09:05 +0000

    Update askthedoc.py

--- askthedoc.py
+++ askthedoc.py
@@ -20,12 +20,18 @@ def split_document(document_text, chunk_size=4096):
 # Function to generate response for a single chunk
 def generate_response(chunk, query_text):
     prompt = f"Document:\n{chunk}\n\nQuestion: {query_text}\nAnswer:"
-    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=100)
-    return response.choices[0].text.strip()
+    response = openai.ChatCompletion.create(
+        model="gpt-4.0-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+    )
+    return response.choices[0].message.content.strip()
 
 # Function to generate response for the entire document
-def generate_full_response(document_text, query_text, chunk_size=4096):
-    chunks = split_document(document_text, chunk_size)
+def generate_full_response(document_text, query_text):
+    chunks = split_document(document_text)
     responses = []
     for chunk in chunks:
         response = generate_response(chunk, query_text)
@@ -41,9 +47,6 @@ uploaded_file = st.file_uploader('Upload a document', type=['pdf', 'doc', 'docx'
 
 # Query text
 query_text = st.text_input('Enter your question:', placeholder='Please provide a short summary.')
-
-# Chunk size input
-chunk_size = st.number_input('Chunk Size', min_value=1024, max_value=8192, value=4096, step=1024)
 
 # Form input and query
 result = []
@@ -62,7 +65,7 @@ with st.form('myform', clear_on_submit=True):
             st.error('Unsupported file format. Please upload a PDF, DOC, DOCX, or TXT file.')
         if document_text:
             with st.spinner('Processing...'):
-                response = generate_full_response(document_text, query_text, chunk_size)
+                response = generate_full_response(document_text, query_text)
                 result.append(response)
 
 if len(result) > 0:
