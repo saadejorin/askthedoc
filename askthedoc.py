34664f113d0da56c1fb9ec0af764d95484bf54b0
dev@flux <saadejorin@gmail.com>
2023-06-29 10:21:45 +0000

    Update askthedoc.py

--- askthedoc.py
+++ askthedoc.py
@@ -1,45 +1,14 @@
 import streamlit as st
-import pytesseract
-import pdfplumber
-from docx import Document
-from langchain.llms import OpenAI
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
+from openai import OpenAIApi
 
-def generate_response(uploaded_file, openai_api_key, query_text):
-    if uploaded_file is not None:
-        file_extension = uploaded_file.name.split(".")[-1]
-        if file_extension == "txt":
-            documents = [uploaded_file.read().decode()]
-        elif file_extension == "pdf":
-            with pdfplumber.open(uploaded_file) as pdf:
-                documents = [page.extract_text() for page in pdf.pages]
-        elif file_extension in ["doc", "docx"]:
-            doc = Document(uploaded_file)
-            documents = [paragraph.text for paragraph in doc.paragraphs]
-        else:
-            st.error("Unsupported file format. Please upload a PDF, DOC, DOCX, or TXT file.")
-            return []
+# Initialize OpenAI API client
+openai_api = OpenAIApi(api_key="YOUR_OPENAI_API_KEY")
 
-        # Split documents into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        texts = text_splitter.create_documents(documents)
-
-        # Select embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-
-        # Create a vectorstore from documents
-        db = Chroma.from_documents(texts, embeddings)
-
-        # Create retriever interface
-        retriever = db.as_retriever()
-
-        # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-
-        return qa.run(query_text, n_results=4)
+# Function to generate response using OpenAI GPT-4
+def generate_response(document_text, query_text):
+    prompt = f"Document:\n{document_text}\n\nQuestion: {query_text}\nAnswer:"
+    response = openai_api.complete(prompt, max_tokens=100)
+    return response.choices[0].text.strip()
 
 # Page title
 st.set_page_config(page_title='🦜🔗 Ask the Doc App')
@@ -57,14 +26,25 @@ with st.form('myform', clear_on_submit=True):
     submitted = st.form_submit_button('Submit', disabled=uploaded_file is None)
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
-            response = generate_response(uploaded_file, openai_api_key, query_text)
-            if response:
-                if len(response) >= 1:
-                    result.extend(response[:1])
+            if uploaded_file is not None:
+                file_extension = uploaded_file.name.split(".")[-1]
+                if file_extension == "txt":
+                    document_text = uploaded_file.read().decode()
+                elif file_extension == "pdf":
+                    document_text = " ".join([page.extract_text() for page in pdfplumber.open(uploaded_file).pages])
+                elif file_extension in ["doc", "docx"]:
+                    document = Document(uploaded_file)
+                    document_text = " ".join([paragraph.text for paragraph in document.paragraphs])
                 else:
-                    st.warning("No results found.")
+                    st.error("Unsupported file format. Please upload a PDF, DOC, DOCX, or TXT file.")
+                    st.stop()
             else:
-                st.warning("Error occurred while generating the response.")
+                st.error("Please upload a file.")
+                st.stop()
 
-if result:
+            response = generate_response(document_text, query_text)
+            result.append(response)
+            del openai_api_key
+
+if len(result):
     st.info(result[0])
